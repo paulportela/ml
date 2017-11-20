7b4c34734a78c18acd66bdb809052c44b550c68c
paul <wcoportelap@gmail.com>
2017-11-19 18:35:56 -0800

    finish gradient decent

--- assign1/gradientDescent.m
+++ assign1/gradientDescent.m
@@ -16,10 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+B = [0];
 for i = 1:m
-C = alpha * (theta' * X(i, :)' - y(i))*X(i, :)' / m;
-theta = theta - C;
+C = (theta' * X(i, :)' - y(i))*X(i, :)';
+B = B + C;
 end
+D = alpha * B / m;
+theta = theta - D;
 
     % ============================================================
 
